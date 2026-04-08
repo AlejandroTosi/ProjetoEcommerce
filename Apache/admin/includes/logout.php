f5364d2762925de0fbea29f940c57048099b633c
alejandro <alejandrodtosi@gmail.com>
2026-04-08 05:04:42 -0300

    função de logout para admin

--- Apache/admin/includes/logout.php
+++ Apache/admin/includes/logout.php
@@ -1,15 +1,13 @@
 <?php
-session_start();
+require_once 'auth.php';
 
-if (isset($_SESSION['user_id'])) {
-    // Log out the user by destroying the session
-    session_destroy();
-    // Redirect to the login page or homepage
-    header('Location: /path-to-login-or-homepage');
-    exit();
-} else {
-    // If no user is logged in, redirect to login page
-    header('Location: /path-to-login');
-    exit();
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: index.php');
+    exit;
 }
-?>
+
+session_unset();
+session_destroy();
+
+header('Location: ../index.php');
+exit;
