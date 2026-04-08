cc72c182e0bd767c6e9e5e81e5af48bc824f5b5a
alejandro <alejandrodtosi@gmail.com>
2026-04-08 00:10:59 -0300

    criação do checkout.php

---
+++ Apache/checkout.php
@@ -0,0 +1,7 @@
+<html>  
+    <body>
+        Devido a falta de conhecimento em relação a métodos de pagamento, esta página foi deixada em branco. O processo de checkout e pagamento será implementado futuramente, quando houver uma melhor compreensão sobre o assunto.
+    </body>
+
+
+</html>
