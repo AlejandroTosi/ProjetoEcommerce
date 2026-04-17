57287b7755eac5afb807b9162fa231d8f85733a0
alejandro <alejandrodtosi@gmail.com>
2026-04-17 09:16:38 -0300

    correção da barra lateral

--- Apache/user/includes/barralateral.php
+++ Apache/user/includes/barralateral.php
@@ -1,13 +1,16 @@
 <?php
 $jsonPath = dirname(__DIR__, 2) . "/admin/data/filtros.json";
 $json = file_get_contents($jsonPath);
-$categorias = json_decode($json, true);
+$filtros = json_decode($json, true);
+$categorias = $filtros['categorias'];
 ?>
 
 <div class="barra-lateral">
     <nav class="nav-lateral">
         <?php foreach ($categorias as $categoria): ?>
-            <a href="categoria.php?id=<?= $categoria['id'] ?>" class="sub_menu"><?= $categoria['nome'] ?></a>
+            <a class="sub_menu" href="buscar.php?categoria=<?= $categoria['id'] ?>&q=">
+                <?= htmlspecialchars($categoria['nome']) ?>
+            </a>
         <?php endforeach; ?>
     </nav>
 </div>
